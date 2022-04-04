b8a53b6938aa2cd089b9b732086b2fd7757a5b39
hemilgoyani <goyanihemil8@gmail.com>
2022-04-04 18:37:01 +0530

    WIP: paggination set in filter data

--- car_sale/views.py
+++ car_sale/views.py
@@ -1,13 +1,49 @@
+from re import search
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-
+from .models import CarSaler
 from car_sale.models import CarSaler
 from .forms import CarSaleForm
+from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q
+
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'home.html')
+    print(request.GET.get("search"))
+    search_text = request.GET.get('search')
+    print(search_text,"=-=-=-=-=")
+    if search_text:
+        user_list = CarSaler.objects.filter(Q(year__icontains=search_text) | Q(make__icontains=search_text))
+        if user_list:
+            page = request.GET.get('page', 1)
+            paginator = Paginator(user_list,4)
+            try:
+                users = paginator.page(page)
+            except PageNotAnInteger:
+                users = paginator.page(1)
+            except EmptyPage:
+                users = paginator.page(paginator.num_pages)
+
+            return render(request, 'home.html', { 'users': users })       
+        else:
+            return HttpResponse("No results found for your filter")        
+        
+    else:
+        
+        user_list = CarSaler.objects.all().order_by('-id')
+        page = request.GET.get('page', 1)
+        paginator = Paginator(user_list,4)
+        try:
+            users = paginator.page(page)
+        except PageNotAnInteger:
+            users = paginator.page(1)
+        except EmptyPage:
+            users = paginator.page(paginator.num_pages)
+
+        return render(request, 'home.html', { 'users': users })
 
 
 def car_saler_form(request):
@@ -26,5 +62,4 @@ def car_saler_form(request):
 
 def successform(request):
     id = CarSaler.objects.latest('id').id
-    print(id,"=-=-=")
     return render(request, 'success.html',{'upload_form': id})
