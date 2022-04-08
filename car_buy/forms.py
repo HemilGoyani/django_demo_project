d52fa882da0944271d6f22609344eff661d35333
hemilgoyani <goyanihemil8@gmail.com>
2022-04-08 09:14:00 +0530

    save the model data car buyer

--- car_buy/forms.py
+++ car_buy/forms.py
@@ -1,9 +1,19 @@
-from tkinter.tix import Form
 from django import forms
 from .models import CarBuyer
+from django.core.exceptions import ValidationError
 
 
 class CarBuyerForm(forms.ModelForm):
     class Meta:
         model = CarBuyer
         fields = '__all__'
+
+    def asking_price(self):
+        print("check the data")
+        data = self.cleaned_data['asking_price']
+        print(data, "=-=-=")
+        if len(data) < 1000 and len(data) > 100000:
+            raise ValidationError(
+                "price less than 100000 & greater than 1000!")
+
+        return data
